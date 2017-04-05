67a9f3dd82316b9b63fbf15602dbce5cc67afe8a
Taylor Thomas <taylor.thomas2@nike.com>
2017-04-04 17:39:19 -0700

    Adds support for user authentication
    This adds support for username and password authentication along with
    support for MFA workflow. This has unit test coverage and local testing
    against the Cerberus server. The AWS auth methods have also been stubbed
    out for implementation in a future commit.

--- auth/aws.go
+++ auth/aws.go
@@ -1,1 +1,39 @@
 package auth
+
+import (
+	"net/http"
+	"os"
+	"time"
+)
+
+// AWSAuth uses AWS roles and authentication to authenticate to Cerberus
+type AWSAuth struct {
+	token   string
+	region  string
+	roleARN string
+	expiry  time.Time
+}
+
+func NewAWSAuth(cerberusURL, roleARN, region string) (*AWSAuth, error) {
+	return &AWSAuth{}, nil
+}
+
+func (a *AWSAuth) GetToken(f *os.File) (string, error) {
+	return "", nil
+}
+
+func (a *AWSAuth) IsAuthenticated() bool {
+	return len(a.token) > 0 && time.Now().Before(a.expiry)
+}
+
+func (a *AWSAuth) Refresh() error {
+	return nil
+}
+
+func (a *AWSAuth) Logout() error {
+	return nil
+}
+
+func (a *AWSAuth) GetHeaders() (http.Header, error) {
+	return nil, nil
+}
