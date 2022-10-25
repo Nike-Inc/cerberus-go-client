2affb93ca687f694d70b618a9eb9375a020af82f
Shawn Sherwood <shawn-sher@users.noreply.github.com>
2022-10-25 13:10:22 -0700

    Version bumped to 3.0.9

--- v3/api/types.go
+++ v3/api/types.go
@@ -27,7 +27,7 @@ import (
 )
 
 // ClientHeader is the header version for all requests. It should be updated on version bumps
-const ClientHeader = "CerberusGoClient/3.0.8"
+const ClientHeader = "CerberusGoClient/3.0.9"
 
 // AuthStatus is the status of a UserAuthResponse
 type AuthStatus string
